5362633484de4342be1c7469eae6ee75d2fe8fba
CiaranRoche203 <rocheciaran37@gmail.com>
2020-12-31 09:31:07 +0000

    test

--- src/components/editPlayer.js
+++ src/components/editPlayer.js
@@ -1,10 +1,9 @@
 import React from 'react';
 import axios from 'axios';
-//create class of the app
-export class CreatePlayer extends React.Component {
-    //create component
+//edit class of the app
+export class EditPlayer extends React.Component {
+    //edit component
     //constructor
-    //
     constructor() {
         super();
         //binding the data so it can be called
@@ -21,6 +20,25 @@ export class CreatePlayer extends React.Component {
             Image: ''
         }
     }
+    //life cycle hook that gets fired. Becomes active in the view
+    componentDidMount(){
+        console.log(this.props.match.params.id);
+        //read record from database
+        //call get request and return to document
+        axios.get('http://localhost:4000/api/myteam/'+ this.props.match.params.id)
+        .then(response =>{
+            this.setState({
+                _id:response.data._id,
+                Name:response.data.name,
+                Age:response.data.age,
+                Number:response.data.number,
+                Image:response.data.image
+            })
+        })
+        .catch((error)=>{
+            console.log(error);
+        });
+    }
     //methods
     //when the name has been added it is returned or stored here
     onChangeName(e) {
@@ -34,6 +52,7 @@ export class CreatePlayer extends React.Component {
     onChangeImage(e) {
         this.setState({ Image: e.target.value });
     }
+    //the number that is added is stored here in the target value
     onChangeNumber(e) {
         this.setState({ Number: e.target.value });
     }
@@ -46,36 +65,28 @@ export class CreatePlayer extends React.Component {
             name: this.state.Name,
             age: this.state.Age,
             number: this.state.Number,
-            image: this.state.Image
+            image: this.state.Image,
+            _id: this.state._id
         }
-        //using the local host aka the server to retrieve the information
-        axios.post('http://localhost:4000/api/myteam',newPlayer)
-        .then((res)=>{
-            console.log(res);
+        //put method. this returns the updated player
+        axios.put('http://localhost:4000/api/myteam/'+this.state._id, newPlayer)
+        .then(res =>{
+            console.log(res.data)
         })
-        .catch((err)=>{
-            console.log(err);
-        });
+        .catch();
     }
     render() {
         return (
             //forms that have been made with react features
-            //add players name, age, number and poster
-            <div className='App' style={{
-                display: "flex",
-                justifyContent: "center",
-                backgroundImage: "url(" +
-                "https://wallpaperaccess.com/full/1127317.jpg" + ")",
-                width: '1903px',
-        height: '1000px',
-                alignItems: "top"}}>
+            //edit player name, age, squad number and poster
+            <div className='App'>
                 <form onSubmit={this.onSubmit}>
-                    <div className='form-group'>
+                <div className='form-group'>
                         <label>
                             Player Name:
                     </label>
                         <input type='text'
-                            className='form-control form-control-lg'
+                            className='form-control'
                             value={this.state.Name}
                             onChange={this.onChangeName}>
                         </input>
@@ -83,7 +94,7 @@ export class CreatePlayer extends React.Component {
                     <div className='form-group'>
                         <label> Player Age</label>
                         <input type='text'
-                            className='form-control form-control-lg'
+                            className='form-control'
                             value={this.state.Age}
                             onChange={this.onChangeAge}>
                         </input>
@@ -91,7 +102,7 @@ export class CreatePlayer extends React.Component {
                     <div className='form-group'>
                         <label>Squad Number</label>
                         <textarea type='text'
-                            className='form-control form-control-lg'
+                            className='form-control'
                             value={this.state.Number}
                             onChange={this.onChangeNumber}>
                         </textarea>
@@ -99,15 +110,15 @@ export class CreatePlayer extends React.Component {
                     <div className='form-group'>
                         <label>Player Image</label>
                         <textarea type='text'
-                            className='form-control form-control-lg'
+                            className='form-control'
                             value={this.state.Image}
                             onChange={this.onChangeImage}>
                         </textarea>
                     </div>
                     <div className='form-group'>
                         <input type='submit'
-                            value='Add Player'
-                            className='btn btn-success btn-lg btn-block'
+                            value='Edit Player'
+                            className='btn btn-primary'
                         ></input>
 
                     </div>
